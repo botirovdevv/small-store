7a637d96f53453e616c89102a4f6cfbf37dcbff4
botirovdev <botirovdev7@gmail.com>
2024-05-11 19:40:11 +0500

    some edits

--- src/components/main/Products.jsx
+++ src/components/main/Products.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import Backet from '../../pages/Backet'
+// import Backet from '../../pages/Backet'
 import axios from 'axios'
+import { Button, Paper } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Products() {
 
     const [cartItems, setCartItems] = useState([])
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(false)
+
 
     useEffect(() => {
         setLoading(true)
@@ -20,13 +23,42 @@ function Products() {
         const productToAdd = products.find((product) => product.id === productId)
 
         if (productToAdd) {
-            setCartItems((prevItems) => [...prevItems, productToAdd])
+            setCartItems((prevItems) => {
+                const duplicateCount = prevItems.filter((item) => item.id === productId).length;
+
+                if (duplicateCount >= 1) {
+                    return prevItems.map(item => {
+                        if (item.id === productId) {
+                            return { ...item, quantity: item.quantity + 1 };
+                        }
+                        return item;
+                    });
+                } else {
+                    return [...prevItems, { ...productToAdd, quantity: 1 }];
+                }
+            });
         }
-        console.log(cartItems);
     }
 
     const removeToCart = (productId) => {
-        setCartItems((prevItems) => prevItems.filter((item) => item.id !== productId))
+        setCartItems((prevItems) => {
+            const duplicateCount = prevItems.filter((item) => item.id === productId).length;
+
+                if (duplicateCount >= 1) {
+                    return prevItems.map(item => {
+                        if (item.id === productId) {
+                            return { ...item, quantity: item.quantity - 1 };
+                        }
+                        return item;
+                    });
+                } else {
+                    return [...prevItems, { ...productToAdd, quantity: 1 }];
+                }
+        })
+    }
+
+    const clearAll = () => {
+        setCartItems([])
     }
 
     return (
@@ -34,42 +66,46 @@ function Products() {
             {
                 loading ?
 
-                    <h1>Loading</h1>
+                    <div className="products-loader">
+                        <h1>Loading</h1>
+                    </div>
 
                     :
                     <section className="products">
                         <div className="container">
                             <div className="products-cards">
                                 {products.map((item, key) => (
-                                    <div className="products-card" key={item.id}>
+                                    <div className="backet-card" key={item.id}>
                                         <div className="products-image">
                                             <img src={item.image} className='products-img' alt="" />
                                         </div>
-                                        {/* <h1 className='products-name'>{item.title}</h1> */}
+                                        <h1 className='products-name'>{item.title.substring(0, 10)}...</h1>
                                         <h2 className='products-desc'>{item.category}</h2>
-                                        <h2 className='products-price'>{item.price}</h2>
-                                        <button className='products-btn' onClick={() => addToCart(item.id)}>Add to cart</button>
+                                        <h2 className='products-price'>{item.price}$</h2>
+                                        <Button variant="contained" className='products-btn' onClick={() => addToCart(item.id)}>Add to card</Button>
                                     </div>
                                 ))}
                             </div>
                             <div className="backet">
-                                <div>
-                                    <h1>backet</h1>
-                                    <h1>Total cards: {cartItems.length}</h1>
+                                <h1 className='backet-title'>Backet</h1>
+                                <div className='backet-buttons'>
+                                    <Button variant="contained" onClick={clearAll}>Clear all</Button>
+                                    <Button variant="contained">Total cards: {cartItems.length}</Button>
                                 </div>
-                                <div className="products-cards">
-                                    {cartItems.map((item) => {
-                                        return (
-                                            <div className="products-card">
-                                                <div className="products-image">
-                                                    <img src={item.image} className='products-img' alt="" />
-                                                </div>
-                                                <h2 className='products-desc'>{item.category}</h2>
-                                                <h2 className='products-price'>{item.price}</h2>
-                                                <button className='products-btn' onClick={() => removeToCart(item.id)}>Remove to card</button>
+                                <div className="backet-cards">
+                                    {cartItems.map((item) => (
+                                        <div className="products-card" key={item.id}>
+                                            <div className="products-image">
+                                                <img src={item.image} className='products-img' alt="" />
                                             </div>
-                                        )
-                                    })}
+                                            <h2 className='products-desc'>{item.category}</h2>
+                                            <h2 className='products-price'>{item.price}$</h2>
+                                            <h1>{item.quantity} ta mahsulot</h1>
+                                            <Button variant="outlined" className='basket-delete' color='error' onClick={() => removeToCart(item.id)} startIcon={<DeleteIcon />}>
+                                                Delete
+                                            </Button>
+                                        </div>
+                                    ))}
 
                                 </div>
                             </div>
